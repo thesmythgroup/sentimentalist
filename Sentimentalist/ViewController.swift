89c011b422e2ccce681529f579e6a43bc1701174
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2017-11-14 17:48:39 -0500

    Initial UI

--- Sentimentalist/ViewController.swift
+++ Sentimentalist/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    @IBAction func textDidChange(_ sender: Any) {
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
