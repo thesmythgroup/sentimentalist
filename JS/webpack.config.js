65619c60775099c9b98f866d1cce2ca8b7bb9288
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2022-12-29 14:18:16 +0100

    fix: Update webpack and sentiment to latest versions and fix index.js
    The way sentiment has to be called has changed, which broke the tutorial steps.
    This is now adjusted to the way the latest version works.

--- JS/webpack.config.js
+++ JS/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path')
 
 module.exports = {
+    mode: 'production',
     entry: { Sentimentalist: './index.js' },
     output: {
         path:  path.resolve(__dirname, 'dist'),
